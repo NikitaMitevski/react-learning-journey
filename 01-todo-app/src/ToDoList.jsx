e463332959906f6a2dcb7238a163d52820c6cc41
Nikita Mitevski <mitevskinikita@gmail.com>
2026-01-09 19:03:54 +0100

    Followiong Primeagen's adivce to just build something i build this to do list and made a simple styling

--- 01-todo-app/src/ToDoList.jsx
+++ 01-todo-app/src/ToDoList.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import './ToDoList.css';
 
 function ToDoList() {
 
-    const [tasks, setTasks] = useState(["Eat", "Sleep", "Play"]);
+    const [tasks, setTasks] = useState(["Eat", "Sleep"]);
     const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event) {
@@ -22,8 +23,24 @@ function ToDoList() {
         setTasks(updatedTasks);
     }
 
+    function moveTaskUp(index) {
+        if(index > 0) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
+
+    function moveTaskDown(index) {
+        if(index < tasks.length - 1) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
+
     return(
-    <div>
+    <div className="main">
         <h1>To - Do - Items</h1>
 
         <div>
@@ -31,17 +48,20 @@ function ToDoList() {
             type="text"
             placeholder="Enter Task"
             value={newTask}
-            onChange={handleInputChange} 
+            onChange={handleInputChange}
+            className = "input" 
             />
 
-            <button onClick={addTask}>Add</button>        
+            <button className = "added" onClick={addTask}>Add</button>        
         </div>
 
         <ol>
             {tasks.map((task, index) =>
                 <li key={index}>
                     <p>{task}</p>
-                    <button onClick={() => deleteTask(index)}>Delete</button>
+                    <button onClick={() => deleteTask(index)} className="del">Delete</button>
+                    <button onClick={() => moveTaskUp(index)} className="up">Up</button>
+                    <button onClick={() => moveTaskDown(index)} className="down">Down</button>
                 </li> 
             )}
         </ol>
